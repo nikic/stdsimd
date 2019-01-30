4283eac6b06939e0a5115663799db937dd892906
Travis CI User <travis@example.org>
2019-01-30 13:25:58 +0000

    Update documentation

---
+++ arm/source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["core_arch"] = {"name":"","dirs":[{"name":"arm","dirs":[],"files":["armclang.rs","dsp.rs","mod.rs","v6.rs","v7.rs"]}],"files":["lib.rs","macros.rs","mod.rs","simd.rs","simd_llvm.rs"]};
+sourcesIndex["std_detect"] = {"name":"","dirs":[{"name":"detect","dirs":[{"name":"arch","dirs":[],"files":["arm.rs"]},{"name":"os","dirs":[],"files":["other.rs"]}],"files":["bit.rs","cache.rs","error_macros.rs","mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
